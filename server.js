cd691cc6c7fec1fa7b02c492aa0770477bdcf2ab
aramaram21 <aramaram@live.unc.edu>
2021-12-01 16:15:38 -0500

    third

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.post("/app/new/", (req, res) => {
     const info = stmt.run(req.body.user,md5(req.body.pass));
     //console.log(info.changes);
     res.status(201).json({"message":"1 record created: ID 3 (201)"});
-    console.log(info.changes);
+    //console.log(info.changes);
     //console.log(info.lastInsertRowid);
 	//res.status(200).json(stmt);
 });
