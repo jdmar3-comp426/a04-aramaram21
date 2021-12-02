429f6a94495d6510fcb8fef91b43858d672ec800
aramaram21 <aramaram@live.unc.edu>
2021-12-02 08:58:00 -0500

    third

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get("/app/", (req, res, next) => {
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new/", (req, res) => {	
-	const stmt = db.prepare("INSERT INTO userinfo (user, pass) WHERE id = 3 VALUES (?, ?)");
+	const stmt = db.prepare("INSERT INTO userinfo (user, pass) WHERE id = 3");
     const info = stmt.run(req.body.user,md5(req.body.pass));
     res.status(201).json({"message":"1 record created: ID 3 (201)"});
 
